745d5bb9c724090ea9bffbc407802c5b0d26f5b3
TWOJOB <62547528+TWOJOB@users.noreply.github.com>
2020-04-28 17:16:48 +0900

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "changeofworld.com@gmail.com";
+    $to = 'changeofworld.com@gmail.com';
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
